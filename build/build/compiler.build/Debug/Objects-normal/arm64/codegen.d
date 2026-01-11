c40d1062866b3cf1c0cb82d96b59eccf3e079e15
Alejandro <alejandrorrnn@icloud.com>
2026-01-11 17:49:32 +0100

    Revert "Delete build directory"
    This reverts commit 92e78e558a52b61b4f6c5556be824cb82c183cf5.
    
    sdsdffsdf Changes to be committed:

---
+++ build/build/compiler.build/Debug/Objects-normal/arm64/codegen.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/alejandroregidornova/CLionProjects/compiler/codegen.cpp \
+  /Users/alejandroregidornova/CLionProjects/compiler/codegen.h \
+  /Users/alejandroregidornova/CLionProjects/compiler/ast.h
---
+++ build/build/compiler.build/Debug/Objects-normal/arm64/lexer.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/alejandroregidornova/CLionProjects/compiler/lexer.cpp \
+  /Users/alejandroregidornova/CLionProjects/compiler/lexer.h \
+  /Users/alejandroregidornova/CLionProjects/compiler/ast.h
